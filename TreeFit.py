3e7094be121740db792113e1bc9e7aa584fa611e
nils_reichert <nilschristianreichert@gmail.com>
2016-12-01 16:16:36 -0600

    added files

---
+++ TreeFit.py
@@ -0,0 +1,38 @@
+import py_stringmatching as sm
+import pandas as pd
+from py_stringmatching import utils
+#incomplete list of necessary imports
+#we will need to import the necessary scikit learn packages when we get there
+
+#Initialize the q-gram tokenizer
+qg3_tok_set = sm.QgramTokenizer(qval=3, return_set=True)
+
+#Initialize similarity score calculators
+jac = sm.Jaccard()
+oc = sm.OverlapCoefficient()
+
+#read in the CSV into a DataFrame
+gold_raw_data = pd.read_csv('gold.csv', low_memory=False)
+
+#Extract the gold labels from the DataFrame
+#This becomes an input into our Learning Algs
+gold_labels = gold_raw_data['Match?']
+
+#This is our feature vector table
+#Another input into our Learning Algs.
+#We add feature vectors from with in the for
+#loop that iterates over the DataFrame 
+feature_vector_table = []
+
+for index, row in gold_raw_data.iterrows():
+    #Note: access Attributes as row['attributename']
+
+    #initialize the feature vector
+    feature_vector = []
+
+    #Do all necessary work to build the feature vector
+
+    #Add feature vector to the feature vector table
+    feature_vector_table.append(feature_vector)
+
+#Run Tree Algs using feature_vector_table and gold_labels
