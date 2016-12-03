b150fd59cf12ed61d32da01d6e4e712de17cc7d7
nils_reichert <nilschristianreichert@gmail.com>
2016-12-03 16:31:53 -0600

    nills Stuff

--- TreeFit.py
+++ TreeFit.py
@@ -29,8 +29,41 @@ for index, row in gold_raw_data.iterrows():
 
     #initialize the feature vector
     feature_vector = []
-
+    
     #Do all necessary work to build the feature vector
+
+
+    #Calculate the VIN Similarity Score
+    VIN_Score = 0
+    eVIN = row['Eastvin']
+    wVIN = row['Westvin']
+    if(not(eVIN==None) and not(eVIN=="")  and (isinstance(eVIN, str))):
+        if(not(wVIN==None) and not(wVIN=="")  and (isinstance(wVIN, str))):
+            eVIN_tok = qg3_tok_set.tokenize(eVIN)
+            wVIN_tok = qg3_tok_set.tokenize(wVIN)
+            j = jac.get_raw_score(eVIN_tok, wVIN_tok)
+            o = oc.get_sim_score(eVIN_tok, wVIN_tok)
+            VIN_Score = ((j+o)/2)
+    feature_vector.append(VIN_Score)
+
+
+    #Calculate the Title Similarity Score
+    Title_Score = 0
+    eTitle = row['Easttitle']
+    wTitle = row['Westtitle']
+    if(not(eTitle==None) and not(eTitle=="") and (isinstance(eTitle, str)) ):
+        if(not(wTitle==None) and not(wTitle=="") and (isinstance(wTitle, str)) ):
+            eTitle_tok = qg3_tok_set.tokenize(eTitle)
+            wTitle_tok = qg3_tok_set.tokenize(wTitle)
+            j = jac.get_raw_score(eTitle_tok, wTitle_tok)
+            o = oc.get_sim_score(eTitle_tok, wTitle_tok)
+            Title_Score = ((j+o)/2)
+    feature_vector.append(Title_Score)
+
+    #Calculate the Make Model Similarity Score
+    MakeModel_Score = row['MatchScore']
+    feature_vector.append(MakeModel_Score)
+    
 
     #Add feature vector to the feature vector table
     feature_vector_table.append(feature_vector)
